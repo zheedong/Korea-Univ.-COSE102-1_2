7898d8a406d40a75509ae33c880cca4270b8048c
zheedong <35412648+zheedong@users.noreply.github.com>
2019-09-14 19:52:27 +0900

    Update

---
+++ 9_11/실습/0911-2019320045-지동환-1.java
@@ -0,0 +1,19 @@
+
+public class SystemoutTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 1;
+		double j = 2.2;
+		
+		System.out.print("\n");
+		System.out.println();
+		System.out.print(i + "\n");
+		System.out.println(i);
+		System.out.print(i+j + "\n");
+		System.out.println(i+j);
+		System.out.println("°á°ú="+i+j);
+		System.out.printf("%d+%.1f=%.1f", i, j, i+j);
+	}
+
+}
---
+++ 9_11/실습/0911-2019320045-지동환-2.java
@@ -0,0 +1,16 @@
+
+public class practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 15;
+		int m = 015;
+		int k = 0x15;
+		int b = 0b0101;
+		System.out.println(n);
+		System.out.println(m);
+		System.out.println(k);
+		System.out.println(b);
+	}
+
+}
